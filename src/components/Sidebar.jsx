1d7a466e328f0d1656619a4c74ef70537a9f2380
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-20 12:35:45 -0300

    fix: put autoclosing tag

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export const Sidebar = () => {
             src="https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
         <div className={styles.profile}>
-            <Avatar src='https://github.com/gabrielamenezes.png' hasBorder>
+            <Avatar src='https://github.com/gabrielamenezes.png' hasBorder />
             <strong>Gabriela Menezes</strong>
             <span>Web developer</span>
         </div>
