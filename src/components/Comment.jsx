66b3277710facc50fad2ce0b8056f235e1f826bf
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-21 21:08:33 -0300

    feat: add like button feature

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,16 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 export const Comment = ({content, onDeleteComment}) => {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount => likeCount + 1)
   }
   return (
     <div className={styles.comment}>
@@ -25,9 +32,9 @@ export const Comment = ({content, onDeleteComment}) => {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
